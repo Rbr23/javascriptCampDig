051d3e93b7a3df89db248ed35f8fe35f8293703a
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-04-22 14:32:42 -0300

    aula1

---
+++ Aula Javascript/Variável.js
@@ -0,0 +1,26 @@
+
+
+
+//Uma variável é um espaço que eu abro na memória do computador
+// o sinal de "=" em programação representa atribuição e chamamos de sinal de atribuição
+// Uma variável precisa ter um nome que chamamos de identificador
+// sempre que criamos uma variável e atribuímos um novo valor; será sempre o último que ficará
+// o comando console.log() imprime um resultado na tela
+//javascript é uma linguagem de tipagem dinâmica ou tipagem fraca
+//string é um tipo de texto
+//existem tipos numéricos
+// o sinal de "+" representa soma entre duas variáveis numéricas ou junção entre variáveis (numéricas / string), (string / string)
+
+//numérico
+var numero = 10
+var numero2 = 25
+
+var resultado = numero + numero2
+
+
+//string
+var nome = "Roberto"
+
+
+
+console.log("Minha idade é " + resultado)
