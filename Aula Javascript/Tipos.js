7efc9535a281597767f6898091989b02aae5c093
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-04-24 14:30:19 -0300

    envio da aula de tipagem

--- Aula Javascript/Tipos.js
+++ Aula Javascript/Tipos.js
@@ -3,5 +3,25 @@
 
 //String
 var nome = "João"
+var sobrenome = "Batista"
+//literal é uma string que não está armazenada em uma variável
 
-console.log("Meu Nome é :" + nome)
+//Number
+var idade = 30
+
+//Boolean
+var mensagem = true
+var mensagem = false
+
+//Undefined ou null
+var salario 
+var salario = null
+
+//Object
+var endereco = {
+    rua: "da travessa",
+    nome: 10
+}
+
+//Array
+var frutas = ['abacaxi', 'banana']
