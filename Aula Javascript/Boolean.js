ebf51a35749589fa588be3725903db61110a4b60
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-05-01 09:57:51 -0300

    aula do dia 06.05.20

---
+++ Aula Javascript/Boolean.js
@@ -0,0 +1,45 @@
+
+
+//O tipo boolean se refere a possibilidade de atribuir ou verificar se uma variável ou se uma condição é verdadeira ou falsa
+
+/*
+
+condição = verdadeira
+
+caso condição for verdadeira então eu executo trecho de código {
+
+    calcular o salario
+
+}
+
+caso condição for falsa então eu nao executo trecho de código {
+
+    calcular salario
+}
+
+*/
+
+var condicao = true //variável com valor de verdadeira
+
+var condicao = false //variável com valor de falsa
+
+var numeroValido = 2 > 1 //condição com valor de verdadeira
+numeroValido = 2 > 8 // condição com valor de falsa
+
+console.log(numeroValido)
+
+/*
+
+
+caso (2 > 1 for verdadeira) então eu executo trecho de código {
+
+    calcular o salario
+
+}
+
+caso (2 > 8 for falsa) então eu nao executo trecho de código {
+
+    calcular salario
+}
+
+*/
--- Aula Javascript/Number.js
+++ Aula Javascript/Number.js
@@ -7,10 +7,57 @@
 
 
 var numeroInteiro = 8
-var numeroFlutuante = 8.56
+var numeroFlutuante = 8.7674635539
 
 //console.log(typeof numeroFlutuante)
+//console.log(Number.isInteger(numeroFlutuante)) //retorna se a variável é inteira ou não, através do true (verdadeiro) ou false (falso)
+//console.log(numeroFlutuante.toString()) //Método que transforma uma variável number em tipo string ljhhjhjgjggjs
 
-console.log(Number.isInteger(numeroFlutuante)) //retorna se a variável é inteira ou não, através do true (verdadeiro) ou false (falso)
+var numeroDecimal = 3.487215639
 
-console.log(numeroFlutuante.toString()) //Método que transforma uma variável number em tipo string
+console.log(numeroDecimal.toFixed(0)) //método tofixed(), quando passamos o parâmetro 0, ele arredonda a variável
+
+/* 
+    EX: variável > 7.5 => arredonda para cima = 8
+    EX: variável < 7.5 => arredonda para baixo = 7
+
+*/
+
+console.log(numeroDecimal.toFixed(4)) //tofixed() retorna a quantidade de casas decimais da variável, de acordo com o número que você passar por parâmetro
+
+console.log(numeroDecimal.toPrecision(1)) //retorna a quantidade total de dígitos que eu quero trazer da variável
+
+var numero = Number('25') //comando que nos permite transformar variáveis do tipo string em tipo number
+
+var numero1 = Number('56')
+
+console.log(typeof numero1)
+
+
+
+
+var peso1 = 0.2
+var peso2 = 0.8
+
+var avaliacao1 = 9.67
+var avaliacao2 = 5.87
+
+var total = avaliacao1 * peso1 + avaliacao2 * peso2
+
+var media = total / (peso1 + peso2)
+
+console.log(`A média das avaliacões de nota ${avaliacao1} e nota ${avaliacao2} é: ${media}`)
+
+/*
+
+OS PASSOS ABAIXOS SÃO SOBRE A VARIÁVEL MEDIA.
+
+1) verificar o tipo
+2) uma vez verificado que é do tipo number, conseguir verificar se é inteiro ou flutuante
+3) arredondar o número 
+4) traga a variável apenas com 3 casas decimais
+5) faça a variavel retornar com apenas os 5 primeiros dígitos
+6) transforme a variável em tipo string
+7) verifique se a variável realmente está em string
+
+*/
