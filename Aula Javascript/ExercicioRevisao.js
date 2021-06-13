88516511207f673d24a34f4f37be67cf0796b237
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-06-13 20:55:20 -0300

    revisão quinta

--- Aula Javascript/ExercicioRevisao.js
+++ Aula Javascript/ExercicioRevisao.js
@@ -1,4 +1,5 @@
 
+console.log("EXERCÍCIO SEMANAL SOBRE VARIÁVEL")
 
 //EXERCÍCIO SEMANAL SOBRE VARIÁVEIS
 
@@ -46,7 +47,7 @@ Tipagem dinâmica é a capacidade que a linguagem tem de reconhecer o tipo da va
 */
 
 
-
+console.log("EXERCÍCIO SEMANAL SOBRE STRING")
 
 
 //EXERCÍCIO SEMANAL SOBRE STRING
@@ -83,4 +84,193 @@ Tipagem dinâmica é a capacidade que a linguagem tem de reconhecer o tipo da va
  //4
  var variavelString = `${texto.indexOf('x')}, ${texto.charAt(12)}, ${texto.substring(8, 16)} `
 
- console.log(variavelString)
+//5
+//métodos são os recursos que virão junto com o tipo que eu atribuir àquela variável
+
+
+
+
+
+console.log("EXERCÍCIO SEMANAL SOBRE NUMBER")
+
+//EXERCÍCIO SEMANAL SOBRE NUMBER
+
+//1
+var numero = 4.4
+console.log(Number.isInteger(numero))
+
+//2
+//a)
+var numeroInteiro = 2
+var numeroDecimal = 3.56273
+
+//b)
+console.log(numeroDecimal.toFixed(2))
+
+//c)
+var texto = `O valor ${numeroInteiro} é inteiro porque não tem casas decimais,
+porém,
+caso tivesse as casas decimais poderia ser chamado de float, 
+como o seguinte exemplo ${numeroDecimal}
+`
+
+//3
+//a)
+var valor = numeroDecimal.toPrecision(4)
+
+//b
+console.log(valor.toString())
+
+//c
+console.log(typeof valor)
+
+//4
+numeroDecimal.toPrecision(2)
+
+//5
+
+//a) Boolean é um tipo que só pode atribuir o valor de verdadeiro ou falso a uma variável
+
+//b
+var variavelVerd = true
+var variavelFals = false
+
+
+
+
+
+
+console.log("EXERCÍCIO SEMANAL SOBRE OBJECT")
+
+//EXERCÍCIO SEMANAL SOBRE OBJECT
+
+//1
+//a)
+var funcionario = {}
+
+//b)
+funcionario.nome = "Roberto"
+funcionario.sobrenome = "Ribeiro"
+funcionario.profissao = "Professor"
+funcionario.empregado = true
+funcionario.brasileiro = true
+
+//c)
+funcionario.amigo = {
+    nome:"Marcos",
+    sobrenome: "Macedo"
+}
+
+//d
+console.log(funcionario)
+
+
+//2
+//a)
+var conjunto = {
+    preco: 25,
+    numero: 78,
+    idade: 45,
+    empregado: true,
+    autorizacao: true,
+    //c)
+    familia : {
+
+    }
+}
+
+//3
+var objetos = Object.assign(funcionario, conjunto)
+
+//4
+var chaves = Object.keys(objetos)
+
+//5
+var valores = Object.values(objetos)
+
+//6
+//Até o ponto estudado, object ficou entendido como uma coleção de dados de chaves e valores.
+
+//7
+var mensagem = null
+
+
+
+
+
+
+
+console.log("EXERCÍCIO SEMANAL SOBRE ARRAY")
+
+//EXERCÍCIO SEMANAL SOBRE ARRAY
+
+//1
+var arrayElementos = [[], true, 5, "amigo", false, 9, [4, 5, 6, 1], {nome:"Carlos", conjunto : [4, 3, 2]}]
+
+//2
+//a)
+console.log(arrayElementos[5] % arrayElementos[6][1])
+
+//b
+arrayElementos.splice(3,2, "Maria", "João")
+
+console.log(arrayElementos)
+
+//3
+var variavelA = "9"
+var variavelB = 5
+var variavelC = 9
+var variavelD = 10
+
+//a)
+console.log(variavelA == variavelB)
+//b
+console.log(variavelA != variavelC)
+//c
+console.log(variavelC === variavelA)
+
+//4
+console.log(arrayElementos[5] * arrayElementos[7].conjunto[0])
+
+
+
+
+
+
+
+console.log("EXERCÍCIO SEMANAL SOBRE OPERADORES-FUNÇÃO")
+
+//EXERCÍCIO SEMANAL SOBRE OPERADORES-FUNÇÃO
+
+
+//1
+function funcionarioSalario(nome, salario){
+    console.log(`${nome} recebe ${salario} reais por mês`)
+}
+
+funcionarioSalario("Roberto", 100)
+
+
+//2
+function calcularSalario(nome, diasTrabalhados, diaria){
+        var salarioBruto = diasTrabalhados * diaria
+
+        salarioLiquido = salarioBruto - salarioBruto * 0.2
+
+        console.log(`O fucionario ${nome} recebeu ${salarioLiquido} pelos ${diasTrabalhados} dias trabalhados`)
+
+}
+
+calcularSalario("Roberto", 30, 75)
+
+//3
+var condicaoVerdadeiro = 4 > 2
+var condicaoFalso = 8 > 9
+
+//4
+console.log(condicaoFalso || condicaoVerdadeiro)
+console.log(condicaoVerdadeiro && condicaoFalso)
+
+//5
+console.log(condicaoFalso || !condicaoVerdadeiro)
+console.log(condicaoVerdadeiro && !condicaoFalso)
