06b25f9c4a77fb7a9f55ec433797c3e6c46c4971
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-06-22 21:35:15 -0300

    aula dia 22

--- Aula Javascript/Rascunho.js
+++ Aula Javascript/Rascunho.js
@@ -1,128 +1,53 @@
 
 
-//1
-function retornoString(numero){
-    resultado = ""
-    for(var i = 1; i <= numero; i++){
-          resultado = resultado + " + "
-    }
 
-    return resultado
-
-}
-
-
-console.log(retornoString(100))
-
-
-//2
-var array2 = [2, 6, "amigo", true]
-
-function numeroArray(array){
-    arrayResultado = []
-    for(var i = 0; i < array.length; i++){
-        if(typeof array[i] == 'boolean'){
-            arrayResultado.push(array2[i])
+function numerosPares(numero){
+    contador = 1
+    while(contador <= numero){
+        if(contador % 2 == 0){
+            console.log("Número par:", contador)
+        } else{
+            console.log("Número impar:", contador)
         }
+        contador++
     }
-
-    return arrayResultado
+    
 }
 
-console.log(numeroArray(array2))
+numerosPares(10)
 
 
-//3
-//EXERCÍCIO 4 DO DIA 15
-function valorMaximo(array){
-    max = array[0]
-    for(var i = 0; i < array.length; i++){
-        if(array[i] > max){
-            max = array[i]
-        }
+
+//Questão do Lucas
+var escola = [
+    {aluno:'Bia', notas: 4},
+    {aluno:'Lucas', notas:2},
+    {aluno:'Juliana', notas:3},
+    {aluno:'Ricado', notas:10},
+    {aluno:'Leticia', notas:9}
+ ]
+ var contador = 0
+ var totalAlunos = escola.length
+ var somando = 0
+ 
+ do{
+   somando += escola[contador].notas
+   notaAlunos++
+ }while(contador < totalAlunos)
+ 
+ media = somando / totalAlunos
+ console.log(media)
+
+
+
+
+ //Questão do Lucas
+ function tabudada(dois,cinco){
+    for(var i = dois; i <= cinco ; i++){
+       for(var w = dois; w <= cinco; w++){
+          console.log(`${i} x ${w} = ${i * w}`)
+       }
     }
-
-    return max
-}
-
-//FUNÇÃO ARROW
-console.log(valorMaximo([3, 1, 7, 8, 9]))
-
-var imprimirValor = (array) => {
-    max = array[0]
-    for(var i = 0; i < array.length; i++){
-        if(array[i] > max){
-            max = array[i]
-        }
-    }
-
-    return max
-}
-
-
-//4
-function tabuada(){
-    for(var i = 1; i <= 10; i++){
-        
-        for(var y = 1; y <= 10; y++){
-            console.log(`${i} x ${y} = ${i * y}`)
-        }
-
-        console.log("------------------")
-
-    }
-}
-
-tabuada()
-
-arrayMedia = [3, 4, 5]
-
-//5
-function mediaArray(array){
-
-    //soma = 12
-    soma = null
-    for(var i = 0; i < array.length; i++){
-        soma += array[i]
-    }
-
-    //   media = 12 / 3
-         media = soma / array.length
-
-     console.log("A média é de ", media)
-}
-
-mediaArray(arrayMedia)
-
-
-function reajusteSalarial(salario){
-    reajuste = null
-    switch(salario){
-        case 1:
-            reajuste = 0.25
-            console.log("AAAAAAAAAA")
-            break
-        case salario > 280 && salario <= 700:
-            reajuste = 0.20
-            break
-        case salario > 700 && salario <= 1500:
-            reajuste = 0.15
-            break
-        case salario > 1500:
-            reajuste = 0.10
-            break            
-    }
-
-    salarioNovo = salario + salario * reajuste
-    return `Percentual aplicado: ${reajuste}
-            Salário antes do aumento: ${salario}
-            Salário antes do aumento: ${salario * reajuste}
-            Novo salário após o aumento ${salarioNovo}`
-
-}
-
-console.log(reajusteSalarial(1))
-
-
-
-
+ }
+ 
+ tabudada(2,5)
