a36a483dd8e861910fe37bd6fe05a915522be0a6
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-07-02 12:21:11 -0300

    correcao

--- Aula Javascript/Case1.js
+++ Aula Javascript/Case1.js
@@ -2,55 +2,37 @@
 
 clientes = [
 
-{cliente: "Cladio", codigo:8, preco:5.50, taxa:0.7}, //0
-{cliente: "Roberto", codigo:3, preco:3.50, taxa:0.8},
-{cliente: "Marcos", codigo:80, preco:19.50, taxa:0.3},
-{cliente: "Susie", codigo:25, preco:32,taxa:0.5},
-{cliente: "Marta", codigo:98, preco:25, taxa:0.1},
-{cliente: "Julia", codigo:43, preco:3, taxa:0.7}
+{cliente: "Cladio", codigo:8, preco:5.50, taxa:0.07}, 
+{cliente: "Roberto", codigo:3, preco:3.50, taxa:0.08},
+{cliente: "Marcos", codigo:80, preco:19.50, taxa:0.03},
+{cliente: "Susie", codigo:25, preco:32,taxa:0.05},
+{cliente: "Marta", codigo:98, preco:25, taxa:0.01},
+{cliente: "Julia", codigo:43, preco:3, taxa:0.07}
 
 ]
 
 
-clientes2 = [
 
-    {cliente: "Cladio", codigo:8, preco:5.50, taxa:0.7}, //0
-    {cliente: "Roberto", codigo:3, preco:3.50, taxa:0.8},
-    {cliente: "Marcos", codigo:80, preco:19.50, taxa:0.3},
-    {cliente: "Susie", codigo:25, preco:32,taxa:0.5},
-    {cliente: "Marta", codigo:98, preco:25, taxa:0.1},
-    {cliente: "Julia", codigo:43, preco:3, taxa:0.7}
-    
-    ]
 
-    clientes3 = [
 
-        {cliente: "Cladio", codigo:8, preco:5.50, taxa:0.7}, //0
-        {cliente: "Roberto", codigo:3, preco:3.50, taxa:0.8},
-        {cliente: "Marcos", codigo:80, preco:19.50, taxa:0.3},
-        {cliente: "Susie", codigo:25, preco:32,taxa:0.5},
-        {cliente: "Marta", codigo:98, preco:25, taxa:0.1},
-        {cliente: "Julia", codigo:43, preco:3, taxa:0.7}
-        
-        ]    
     
 
 
 
 function gerenciarLanchonete(obj){
-    soma = null
-    imposto = null
+    soma = null  
+    imposto = null 
     for(let i = 0; i < obj.length; i++){
    
         if(obj[i].codigo == 8){
 
-        soma += obj[i].preco
+        soma = soma +  obj[i].preco
         imposto = imposto + (obj[i].preco * obj[i].taxa)
         console.log(obj[i].cliente, " gastou R$", obj[i].preco)
 
     } else if (obj[i].codigo == 3){
 
-        soma += obj[i].preco 
+        soma = soma + obj[i].preco 
         imposto = imposto + (obj[i].preco * obj[i].taxa)
         console.log(obj[i].cliente, " gastou R$", obj[i].preco)
 
@@ -90,46 +72,7 @@ e o imposto pago é de ${imposto}`)
 
 }
 
-gerenciarLanchonete(clientes2)
-
-gerenciarLanchonete(clientes3)
-
 gerenciarLanchonete(clientes)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
