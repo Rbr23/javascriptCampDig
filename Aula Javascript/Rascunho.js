839a8f760ef7803286942b9a6f42f06392778456
Roberto Batista <roberto.ribeiro@al.infnet.edu.br>
2021-07-18 17:16:57 -0300

    formulario

--- Aula Javascript/Rascunho.js
+++ Aula Javascript/Rascunho.js
@@ -1,144 +1,88 @@
 
 
-
-
-/*
-
-  POO
-
-  .ABSTRAÇÃO: 
-
-
-  CARRO: 
-  marca (características - atributo)
-  modelo (características - atributo)
-  ano (características - atributo)
-
-  ligar (comportamento - método)
-
-  desligar (comportamento - método)
-
-  .ENCAPSULAMENTO:
-
-*/
-
-/*
-const FOX = {
-    _marca: "FORD",
-    _modelo: "KA",
-    _ano: 2009,
-
-    get marca(){
-        return this._marca
-    },
-
-    set marca(valor){
-        this._marca = valor
-    },
-    
-
-    ligar:function(desligar){
-        if(desligar > 0){
-            console.log("Carro está ligado")
-    }
-    },
-
-    desligar:function(){
-        console.log("Carro está desligado!")
-    }
-
-}
-
-
-
-
-const BMW = {
-    _marca: "BMW",
-    _modelo: "X9",
-    _ano: 2020,
-
-    get marca(){
-        return this._marca
-    },
-
-    set marca(valor){
-        this._marca = valor
-    },
-    
-
-    ligar:function(desligar){
-        if(desligar > 0){
-            console.log("Carro está ligado")
-    }
-    },
-
-    desligar:function(){
-        console.log("Carro está desligado!")
-    }
-
-}
-
-
-*/
-
-
-
-class carro{
-    #marca
-    #modelo
-    #ano
-    constructor(marca, modelo, ano){
-        this.#marca = marca
-        this.#modelo = modelo
-        this.#ano = ano
-    }
-
-    get marca(){
-        return this.#marca
-    }
-
-    set marca(valor){
-        this.#marca = valor
-    }
-
-    get modelo(){
-        return this.#modelo
-    }
-
-    set modelo(valor){
-        this.#modelo = valor
-    }
-    
-
-}
-
-
-const fox = new carro("Volkswagen", "Fox", 2009)
-
-const fusca = new carro("Volkswagen", "fusca", 1990)
-
-const bmw = new carro("BMW", "X9", 2020)
-
-
-console.log(fox.modelo)
-
-
-
-
-class pessoa{
-    #nome
+//classe pai
+class animal{
+    #tamanho
+    #peso
     #idade
-    #profissao
-    #bipede
-    #condicaoFisica
-    constructor(nome, idade, profissao, condicaoFisica){
-        this.#nome = nome
+    constructor(tamanho, peso, idade){
+        this.#tamanho = tamanho
+        this.#peso = peso
         this.#idade = idade
-        this.#profissao = profissao
-        this.bipede = true
-        this.#condicaoFisica = condicaoFisica
     }
 
+    get tamanho(){
+        return this.#tamanho
+    }
+
+    set tamanho(valor){
+        this.#tamanho = valor
+    }
+
+    get peso(){
+        return this.#peso
+    }
+
+    set peso(valor){
+        this.#peso = valor
+    }
+
+    get idade(){
+        return this.#idade
+    }
+
+    set idade(valor){
+        this.#idade = valor
+    }
+
+    fazerBarulho(){
+        console.log("Fazendo barulho!")
+    }
+
+    comer(){
+        console.log("Comendo!")
+    }
+
+    dormir(){
+        console.log("Dormindo!")
+    }
+}
+
+
+
+//classe filho
+class felino extends animal{
+    #nome
+    constructor(nome, tamanho, peso, idade ){
+        super(tamanho, peso, idade)
+        this.#nome = nome
+    }
+
+    get nome(){
+        return this.#nome
+    }
+    
+    set nome(valor){
+        this.#nome = valor
+    }
+
+    fazerBarulho(){
+        console.log("Comece a miar!")
+    }
+
+    vagar(){
+        console.log("Estou vagando!")
+    }
+}
+
+//classe filho
+class canino extends animal{
+    #nome
+    constructor(nome, tamanho, peso, idade){
+        super(tamanho, peso, idade)
+        this.#nome = nome
+    }
+    
     get nome(){
         return this.#nome
     }
@@ -147,39 +91,19 @@ class pessoa{
         this.#nome = valor
     }
 
-    get idade(){
-        return this.#idade
+    fazerBarulho(){
+        console.log("Comece a latir!")
     }
 
-
+    vagar(){
+        console.log("Estou vagando!")
+    }
 }
 
 
+const leao = new felino("rei da floresta", 1.30, 200, 30)
 
-const roberto = new pessoa("Robyrto", 29, "Professor")
-
-const ligia = new pessoa("ligia", 29, "Coordenadora")
-
-const susie = new pessoa("Susie", 29, "Monitora")
-
-console.log(roberto.nome = "Roberto")
-
-
-
-
-function imprimirNome(nome){
-    console.log(nome)
-}
-
-imprimirNome("Roberto")
-
-
-
-
-function teste(){
-    this.nome = 12
-
-}
+console.log(leao.fazerBarulho())
 
 
 
